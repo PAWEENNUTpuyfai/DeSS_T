0581edae2cc2356c2f0b716253c5a8177f5d5381
Issawara <kmind46@gmail.com>
2025-12-07 16:28:11 +0700

    landing page & guest decision

--- DeSS_T_Frontend/src/app/App.tsx
+++ DeSS_T_Frontend/src/app/App.tsx
@@ -1,21 +1,16 @@
 import { Routes, Route, Link } from "react-router-dom";
-import PowerPage from "./components/PowerPage";
+// import PowerPage from "./components/PowerPage";
+import LandingPage from "./components/LandingPage";
 import "../style/App.css";
 import GuestSetup from "./pages/GuestSetup";
+import GuestDecision from "./components/GuestDecision";
 
 export default function App() {
   return (
     <div className="main-container">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <h1>ข้างนอก</h1>
-              <PowerPage />
-            </>
-          }
-        />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/guest/decision" element={<GuestDecision />} />
         <Route path="/guest/setup" element={<GuestSetup />} />
       </Routes>
     </div>
---
+++ DeSS_T_Frontend/src/app/components/GuestDecision.tsx
@@ -0,0 +1,41 @@
+import "../../style/GuestDecision.css";
+import { useNavigate } from "react-router-dom";
+
+export default function GuestDecision() {
+  const navigate = useNavigate();
+
+  const handleSecondaryClick = () => {
+    navigate("/guest/setup");
+  };
+
+  return (
+    <div className="decision_container m-10">
+      <div className="decision_logobar">
+        <img src="/DeSS-T_logo.png" alt="DeSS-T Logo" className="logo" />
+      </div>
+      <div className="decision_content mb-14">
+        <span className="header flex mb-6">
+          You are currently in{" "}
+          <p className="font-bold text-[#81069E] ml-2">Guest Mode.</p>
+        </span>
+        <span className="question flex">
+          Do you want to download a{" "}
+          <p className="text-[#81069E] mx-2">Configuration Data</p>
+        </span>
+        <span className="question flex">
+          or <p className="text-[#81069E] mx-2">Project</p> from Community
+          workspace?
+        </span>
+        <div className="btn_primary w-[50%] mt-6 flex justify-center items-center">
+          <p>Yes, I want to go to community page.</p>
+        </div>
+        <div
+          className="btn_secondary w-[50%] mt-4 flex justify-center items-center"
+          onClick={handleSecondaryClick}
+        >
+          <p>No, I want to set up my own.</p>
+        </div>
+      </div>
+    </div>
+  );
+}

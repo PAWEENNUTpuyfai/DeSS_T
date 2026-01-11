0c7a91e0c8b321598dd8234945090a736b5ca03e
Issawara <kmind46@gmail.com>
2026-01-11 13:09:33 +0700

    deleted model

--- DeSS_T_Backend-go/models/configuration.go
+++
@@ -1,7 +0,0 @@
-// package models
-
-// type Configuration struct {
-// 	NetworkModel             NetworkModel    `json:"Network_model"`
-// 	AlightingDistribution    DataFitResponse `json:"Alighting_Distribution"`
-// 	InterarrivalDistribution DataFitResponse `json:"Interarrival_Distribution"`
-// }

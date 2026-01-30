ec08b64af3fba520b03df3457ce0e39df169b581
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2026-01-30 16:16:38 +0700

    http for dev

--- DeSS_T_Frontend/src/utility/config.ts
+++ DeSS_T_Frontend/src/utility/config.ts
@@ -1,4 +1,4 @@
 // src/utility/config.ts
-// export const API_BASE_URL = "http://localhost:8080/api";
-export const API_BASE_URL = "http://40.81.22.119:8080/api";
+export const API_BASE_URL = "http://localhost:8080/api";
+// export const API_BASE_URL = "http://40.81.22.119:8080/api";
 // export const API_BASE_URL = "http://backend-go:8080/api";

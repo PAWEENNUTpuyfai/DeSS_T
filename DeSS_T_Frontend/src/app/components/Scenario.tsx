b5ee584218095deff924f0b84b1d4e6fe03a3d2d
Issawara <kmind46@gmail.com>
2026-02-04 21:53:52 +0700

    comment dowload JSON

--- DeSS_T_Frontend/src/app/components/Scenario.tsx
+++ DeSS_T_Frontend/src/app/components/Scenario.tsx
@@ -705,18 +705,18 @@ export default function Scenario({
       };
 
       // Download JSON to local machine
-      const jsonString = JSON.stringify(simulationRequest, null, 2);
-      const blob = new Blob([jsonString], { type: "application/json" });
-      const url = URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = `simulation-request-${new Date()
-        .toISOString()
-        .replace(/:/g, "-")}.json`;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      URL.revokeObjectURL(url);
+      // const jsonString = JSON.stringify(simulationRequest, null, 2);
+      // const blob = new Blob([jsonString], { type: "application/json" });
+      // const url = URL.createObjectURL(blob);
+      // const link = document.createElement("a");
+      // link.href = url;
+      // link.download = `simulation-request-${new Date()
+      //   .toISOString()
+      //   .replace(/:/g, "-")}.json`;
+      // document.body.appendChild(link);
+      // link.click();
+      // document.body.removeChild(link);
+      // URL.revokeObjectURL(url);
 
       const response = await runSimulation(simulationRequest);
       setSimulationResponse(response);

86c8d030660aed9a5b56359252e925c1c72482ab
Issawara <kmind46@gmail.com>
2026-02-14 22:36:56 +0700

    upload img test

--- DeSS_T_Backend-go/controllers/database_controller.go
+++ DeSS_T_Backend-go/controllers/database_controller.go
@@ -4,6 +4,9 @@ import (
 	"DeSS_T_Backend-go/config"
 	"DeSS_T_Backend-go/model_database"
 	"DeSS_T_Backend-go/models"
+	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -76,4 +79,50 @@ func GetGoogleUsers(c *fiber.Ctx) error {
     var users []model_database.User
     config.DB.Find(&users)
     return c.JSON(users)
+}
+
+func UploadTestImage(c *fiber.Ctx) error {
+    uploadDir := os.Getenv("UPLOAD_DIR")
+    if uploadDir == "" {
+        uploadDir = "./uploads"
+    }
+    resolvedUploadDir, err := filepath.Abs(uploadDir)
+    if err != nil {
+        return c.Status(500).JSON(fiber.Map{"error": "cannot resolve upload dir", "detail": err.Error()})
+    }
+
+    if err := os.MkdirAll(resolvedUploadDir, 0o755); err != nil {
+        return c.Status(500).JSON(fiber.Map{"error": "cannot create upload dir", "detail": err.Error()})
+    }
+
+    f, err := c.FormFile("file")
+    if err != nil {
+        return c.Status(400).JSON(fiber.Map{"error": "file missing"})
+    }
+
+    baseName := filepath.Base(f.Filename)
+    ext := filepath.Ext(baseName)
+    if ext == "" {
+        ext = ".bin"
+    }
+
+    fileName := fmt.Sprintf("%d%s", time.Now().UnixNano(), ext)
+    savePath := filepath.Join(resolvedUploadDir, fileName)
+
+    if err := c.SaveFile(f, savePath); err != nil {
+        return c.Status(500).JSON(fiber.Map{"error": "cannot save file", "detail": err.Error()})
+    }
+
+    // Build public URL
+    scheme := "http"
+    if c.Protocol() == "https" {
+        scheme = "https"
+    }
+    baseURL := fmt.Sprintf("%s://%s", scheme, c.Hostname())
+    fileURL := fmt.Sprintf("%s/uploads/%s", baseURL, fileName)
+
+    return c.JSON(fiber.Map{
+        "fileName": fileName,
+        "url":      fileURL,
+    })
 }
--- DeSS_T_Backend-go/routes/database_routes.go
+++ DeSS_T_Backend-go/routes/database_routes.go
@@ -1,11 +1,13 @@
 package routes
 
 import (
-    "github.com/gofiber/fiber/v2"
-    "DeSS_T_Backend-go/controllers"
+	"DeSS_T_Backend-go/controllers"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func DatabaseRoutes(app *fiber.App) {
     api := app.Group("/api")
     api.Post("/auth/google/login", controllers.CreateGoogleUser)
+    api.Post("/upload/test", controllers.UploadTestImage)
 }
